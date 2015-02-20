5f1a67f3b98094424b814021ec0e66fa71211c07
tcstool <russellbutturini@MacBook-Air.local>
2015-02-19 21:59:29 -0600

    Added brute force host selection

--- brutemonkey.py
+++ brutemonkey.py
@@ -2,8 +2,11 @@
 
 import paramiko
 import ftplib
+import telnetlib
 import socket
 import time
+from pymongo import MongoClient
+from random import randint
 
 def findLoginBoxes(runTime,dbIp,dbName,monkeyIq,monkeyLoc,monkeyId):
     timeout = time.time() + 60 * runTime
@@ -13,10 +16,53 @@ def findLoginBoxes(runTime,dbIp,dbName,monkeyIq,monkeyLoc,monkeyId):
         ports = []
         time.sleep(1)
 
+        if time.time() > timeout:
+            break
+
+        conn = MongoClient(dbIp,27017)
+        db = conn[dbName]
+        hosts = db.hosts
 
 
+        if hosts.find({'location':monkeyLoc}).count() == 0:
+            print 'Brute monkey is waiting for work.  Eating bananas.  Will check again in 10 seconds.'
+            time.sleep(10)
 
-def sshBute(targets,db,coll,monkeyId):
+        else:
+            for work in hosts.find({'location':monkeyLoc}):
+                if 21 in work['ports']:
+                    targets.append(work['ip'])
+                    ports.append(21)
+
+                if 22 in work['ports']:
+                    targets.append(work['ip'])
+                    ports.append(22)
+
+                if 23 in work['ports']:
+                    targets.append(work['ip'])
+                    ports.append(23)
+
+
+        if len(targets) == 0:
+            print 'Brute monkey is waiting for something to brute force.  Eating bananas.  Will check again in 10 seconds.'
+            time.sleep(10)
+
+        else:
+            print 'Brute monkey got work! Starting credential brute forcing!'
+
+            index = randint(0,len(targets)-1)
+            brutePort = ports[index]
+
+            if brutePort == 21:
+                ftpBrute(targets[index],db,hosts,monkeyId)
+
+            elif brutePort == 22:
+                sshBrute(targets[index],db,hosts,monkeyId)
+
+            elif brutePort == 23:
+                telBrute(targets[index],db,hosts,monkeyId)
+
+def sshBrute(victim,db,coll,monkeyId):
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
@@ -29,4 +75,9 @@ def sshBute(targets,db,coll,monkeyId):
     except socket.error:
         pass
 
+def ftpBrute(victim,db,coll,monkeyId):
+    print 'ftp bruting goes here.'
 
+
+def telBrute (victim,db,coll,monkeyId):
+    print 'telnet bruting goes here.'
